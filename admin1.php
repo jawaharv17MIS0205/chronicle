2e4d337507f4b41cba189bf21819773491684ccd
Jawahar <57182686+jawaharv17MIS0205@users.noreply.github.com>
2020-04-22 17:14:51 +0530

    Finally

--- admin1.php
+++ admin1.php
@@ -11,7 +11,7 @@ $result2=mysqli_query($con,$s2);
 echo "Sno"."\n"."Email"."\n"."Password"."\n"."Dateofenter"."\n"."Dayofenter"."\n"."Timeofenter"."\n"."Content"."\n";
 if (mysqli_num_rows($result2) > 0){
 	while($row = mysqli_fetch_assoc($result2)) {
-		echo $row['sno']."\n".$row['email']."\n".$row['password']."\n".$row['dateofenter']."\n".$row['dayofenter']."\n".$row['timeofenter']."\n".$row['content']."\n";
+		echo $row['sno']."\n".$row['email']."\n".$row['password']."\n".$row['dateofenter']."\n".$row['dayofenter']."\n".$row['timeofenter']."\n".$row['content']."\n"."<br>";
     }
 } 
 else {
