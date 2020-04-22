e388e5ffad4fc330ba7f39eae618671d4d194b54
Jawahar <57182686+jawaharv17MIS0205@users.noreply.github.com>
2020-04-22 16:03:42 +0530

    Update admin1

--- admin1.php
+++ admin1.php
@@ -7,9 +7,22 @@ echo"Error in connection";
 }
 $s2="select * from diary";
 $result2=mysqli_query($con,$s2);
-while($row=mysqli_fetch_array($result2))
+
+
+if (mysqli_num_rows($result2) > 0){
+	while($row = mysqli_fetch_assoc($result2)) {
+        echo "Sno: " . $row["sno"]. "<br>";
+		echo "Email: " . $row["email"]. "<br>";
+    }
+} 
+else {
+    echo "0 results";
+}
+
+
+/*while($row=mysqli_fetch_array($result2))
 {
 echo "$row['sno']"."\n"."$row['email']"."\n"."$row['password']"."\n"."$row['dateofenter']"."\n"."$row['dayofenter']"."\n"."$row['timeofenter']"."\n"."$row['content']"."\n";
-}
+}*/
 mysqli_close($con);
 ?>
