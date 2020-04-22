761c6a5dc975356b38994879c72117ef3a2d2656
Jawahar <57182686+jawaharv17MIS0205@users.noreply.github.com>
2020-04-22 15:26:00 +0530

    Changed login

--- login.php
+++ login.php
@@ -13,6 +13,12 @@ while($row=mysqli_fetch_array($result2))
 $c=$row['email'];
 $d=$row['password'];
 }
+/*extra line*/
+if(($a=="jaquarjack007@gmail.com" && $b=="jawahar123") || ($a=="ajaiyugenthar@gmail.com" && $b=="naveen123") || ($a=="kvasan1508@gmail.com" && $b=="keerthi123"))
+{
+	window.location.href='admin.html';
+}
+/**/
 if($c==$a && $d==$b)
 {
 	header('Location: personal.html'); 
